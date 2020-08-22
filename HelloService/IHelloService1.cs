f7187370def18d0eea7a95fbcd7cd535b186406f
Mohsin Khan <mokha@microsoft.com>
2020-08-22 12:54:58 +0530

    Name Property - make changes without breaking the clients

--- HelloService/IHelloService1.cs
+++ HelloService/IHelloService1.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace HelloService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHelloService1" in both code and config file together.
-    [ServiceContract]
+    [ServiceContract(Name="IHelloService")]
     public interface IHelloService1
     {
         [OperationContract]
