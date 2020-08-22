d0247bababab4ba8bb26076c2f83fe2549689080
Mohsin Khan <mokha@microsoft.com>
2020-08-22 12:43:50 +0530

    Add project files.

---
+++ HelloService/HelloService1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService1" in both code and config file together.
+    public class HelloService1 : IHelloService1
+    {
+        public void DoWork()
+        {
+        }
+
+        public string GetMessage(string name)
+        {
+            return "Hello  " + name;
+        }
+    }
+}
